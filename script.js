d8a988ba47b003b33d3b44a80233654ac9896073
MiguelRivera03 <162446790+MiguelRivera03@users.noreply.github.com>
2024-03-26 16:20:22 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+var messageDiv = document.getElementById("message");
+
+// Define el mensaje a mostrar
+var mensaje = "¡Hola, mundo! Este es un mensaje desde JavaScript.";
+
+messageDiv.textContent = mensaje;
